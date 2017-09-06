3fdc93033ffcf0b4e2c2328b542112533e13bf1f
Sheayun Lee <sheayun@kookmin.ac.kr>
2017-09-06 11:13:07 +0900

    Commit message header
    - Add a line of comment
    - Ann another line of comment

--- test3_0.py
+++ test3_0.py
@@ -2,9 +2,13 @@ wordcount = {'dog':3, 'cat':5, 'tiger':3}
 print (wordcount['dog'])
 wordcount['dog'] += 1
 word = input("Word: ")
+
+# You need to comment appropriately.
 print (wordcount[word])
 for k in wordcount.keys():
 	print (k)
+
+# Blank lines are sometimes needed.
 for v in wordcount.values():
 	print (v)
 for k, v in wordcount.items():
